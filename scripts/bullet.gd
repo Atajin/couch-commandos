8cc5881a0e99a1197c17bb249d93d2d5d43a3b69
Kian Chowanietz <kianlettres@gmail.com>
2025-02-01 18:48:39 -0500

    Functional bullets

---
+++ scripts/bullet.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+const SPEED = 600.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	position += Vector2.RIGHT.rotated(rotation) * SPEED * delta
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	set_physics_process(false)
+	
+	
+func reset() -> void:
+	show()
+	set_physics_process(true)
