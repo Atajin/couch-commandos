fd1b1578e8b756655ea52bf64d4962a8fe890105
Jean-Mermoude <mermoudeduchamps@gmail.com>
2025-02-01 15:48:03 -0500

    MainMenuV1

---
+++ MainMenu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_start_game_button_down():
+	print("zizi") # Replace with function body.
+
+
+func _on_option_button_down():
+	print("FENT")
+
+
+func _on_quit_button_down():
+	print("Diddy")
