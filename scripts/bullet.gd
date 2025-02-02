fd1b0b51f246d1484808e89940d194517967065f
Kian Chowanietz <kianlettres@gmail.com>
2025-02-01 23:43:06 -0500

    fix bullets thru wall bug

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -3,9 +3,11 @@ extends Area2D
 const SPEED = 600.0
 const BULLET_DAMAGE = 25
 
+var shotByPlayerOne: bool
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_deferred("monitoring", false)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,13 +18,18 @@ func _physics_process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		var player = body as Player
-		player.gotHit(BULLET_DAMAGE)
+		if player.is_player_1 != shotByPlayerOne:
+			player.gotHit(BULLET_DAMAGE)
+	else:
+		print(body)
 	hide()
+	set_deferred("monitoring", false)
 	set_physics_process(false)
 	$AnimatedSprite2D.stop()
 	
 	
 func reset() -> void:
 	show()
+	set_deferred("monitoring", true)
 	set_physics_process(true)
 	$AnimatedSprite2D.play()
